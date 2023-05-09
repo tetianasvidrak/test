36b7807615e9e5adfa6b8b20e5a7f1e7fd48f489
tetianasvidrak <svidrak.tetiana@gmail.com>
2023-05-09 20:09:01 +0200

    Updates

---
+++ static/js/859.8036a408.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest=self.webpackChunktest||[]).push([[859],{859:function(t,e,n){n.r(e),n.d(e,{default:function(){return r}});var u=n(184),r=function(){return(0,u.jsx)(u.Fragment,{})}}}]);
+//# sourceMappingURL=859.8036a408.chunk.js.map
